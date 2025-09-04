dc4f692b1325e9d34028369359113f96729dd5e4
evrwaeq <evrwaeq@gmail.com>
2025-09-04 16:33:27 -0300

    chore: add babel module resolver

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,17 @@ module.exports = function (api) {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
       'nativewind/babel'
+    ],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['.'],
+          alias: {
+            '@': './src'
+          }
+        }
+      ]
     ]
   }
 }
---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const Login = () => {
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Login</Text>
+    </View>
+  )
+}
+
+export { Login }
