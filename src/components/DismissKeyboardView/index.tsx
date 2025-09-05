e790cce38c423397615c575640e077c8aa6a05b2
evrwaeq <evrwaeq@gmail.com>
2025-09-04 21:25:10 -0300

    feat: add DismissKeyboardView component

---
+++ src/components/DismissKeyboardView/index.tsx
@@ -0,0 +1,22 @@
+import { FC, PropsWithChildren } from 'react'
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  ScrollView,
+  TouchableWithoutFeedback
+} from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const DismissKeyboardView: FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <SafeAreaView className="flex-1 bg-background-primary">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <KeyboardAvoidingView className="flex-1" behavior="padding">
+          <ScrollView>{children}</ScrollView>
+        </KeyboardAvoidingView>
+      </TouchableWithoutFeedback>
+    </SafeAreaView>
+  )
+}
+
+export { DismissKeyboardView }
--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,18 +1,20 @@
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
 import { PublicStackParamsList } from '@/routes/PublicRoutes'
+import { DismissKeyboardView } from '@/components/DismissKeyboardView'
 
 const Login = () => {
   const navigation = useNavigation<StackNavigationProp<PublicStackParamsList>>()
 
   return (
-    <View className="flex-1 items-center justify-center">
+    <DismissKeyboardView>
       <Text>Login</Text>
+      <TextInput className="bg-gray-500 w-full" />
       <TouchableOpacity onPress={() => navigation.navigate('Register')}>
         <Text>Registrar</Text>
       </TouchableOpacity>
-    </View>
+    </DismissKeyboardView>
   )
 }
 
