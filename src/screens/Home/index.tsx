fc6e88ebd0af0ec141750b3662e786549064b483
evrwaeq <evrwaeq@gmail.com>
2025-09-09 18:11:21 -0300

    feat: add AppError

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,15 @@
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { useAuthContext } from '@/context/auth.context'
 
 const Home = () => {
+  const { handleLogout } = useAuthContext()
+
   return (
     <View>
       <Text>Home Screen</Text>
+      <TouchableOpacity onPress={handleLogout}>
+        <Text>Sair</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/shared/api/dt-money.ts
+++ src/shared/api/dt-money.ts
@@ -1,5 +1,6 @@
-import axios from 'axios'
 import { Platform } from 'react-native'
+import axios from 'axios'
+import { AppError } from '../helpers/AppError'
 
 const baseURL = Platform.select({
   android: 'http://10.0.2.2:3001',
@@ -9,5 +10,16 @@ const baseURL = Platform.select({
 const dtMoneyApi = axios.create({
   baseURL
 })
+
+dtMoneyApi.interceptors.response.use(
+  (config) => config,
+  (error) => {
+    if (error.response && error.response.data) {
+      return Promise.reject(new AppError(error.response.data.message))
+    } else {
+      return Promise.reject(new AppError('Falha na requisição'))
+    }
+  }
+)
 
 export { dtMoneyApi }
