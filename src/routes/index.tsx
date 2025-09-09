afcb3541abbe11a4cc9f5c2159c4d39de5525c1d
evrwaeq <evrwaeq@gmail.com>
2025-09-08 23:22:49 -0300

    feat: consuming auth context in routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,20 @@
-import { NavigationContainer } from '@react-navigation/native'
-import { PublicRoutes } from './PublicRoutes'
-import { useCallback, useState } from 'react'
-import { PrivateRoutes } from './PrivateRoutes'
+import { useCallback } from 'react'
 import { SystemBars } from 'react-native-edge-to-edge'
+import { NavigationContainer } from '@react-navigation/native'
+import { useAuthContext } from '@/context/auth.context'
+import { PublicRoutes } from './PublicRoutes'
+import { PrivateRoutes } from './PrivateRoutes'
 
 const NavigationRoutes = () => {
-  const [user, setUser] = useState(undefined)
+  const { user, token } = useAuthContext()
 
   const Routes = useCallback(() => {
-    if (!user) {
+    if (!user || !token) {
       return <PublicRoutes />
     } else {
       return <PrivateRoutes />
     }
-  }, [])
+  }, [user, token])
 
   return (
     <NavigationContainer>
